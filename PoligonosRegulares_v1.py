cad19ac6a3f4bee3780aae5c16fc68f81dbd54bd
Alexandre Cardoso Garcia Leite <alexandrecgleite@gmail.com>
2024-07-16 14:23:19 -0300

    Add files via upload
    Desenha o polígono centralizado na tela.

--- PoligonosRegulares_v1.py
+++ PoligonosRegulares_v1.py
@@ -7,6 +7,8 @@ Created on Tue Jul 16 04:27:06 2024
 Script Name: PoligonosRegulares_v1.py
 """
 import turtle
+import math
+
 t = turtle.Pen()
 t.pencolor('blue')
 turtle.bgcolor('black')
@@ -18,9 +20,26 @@ while (lados_poligono < 3):
         print('Erro: Digite num número maior que 2.')
 
 angulo = (lados_poligono - 2) * 180 / lados_poligono
+angulo_central = 360 / lados_poligono
 
-comp_lado = int(1000 / lados_poligono)
+# quanto maior o número de lados, menor o número das arestas, para caber na tela
+comp_lado_oposto = int(500 / lados_poligono)
 
-for x in range(lados_poligono):
-    t.forward(comp_lado)
-    t.left(180 - angulo)
+# é necessário dividir o polígono em triângulos, para poder centralizá-lo
+comp_lado_adjacente = ( comp_lado_oposto / 2 ) / math.tan( angulo_central / 2 * math.pi / 180 ) 
+
+# move para a direita 90 graus
+t.right(90)
+# levanta a caneta para não escrever enquanto se move primeiro vértice do polígono
+t.penup()
+t.forward( comp_lado_adjacente )
+t.right(90)
+t.forward( comp_lado_oposto / 2 )
+t.right(180)
+# baixa a caneta para desenhar o polígono
+t.pendown()
+
+for x in range( lados_poligono ):
+    t.forward( comp_lado_oposto )
+    t.left( 180 - angulo )
+
